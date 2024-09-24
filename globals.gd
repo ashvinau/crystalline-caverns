fff2624756b553dc150b563621d3e3ef5b47d6e0
Joe Wigdor <joe@wigdor.io>
2024-09-24 01:16:29 -0700

    Sorted layers in prep for lighting system.

--- globals.gd
+++ globals.gd
@@ -6,7 +6,7 @@ const NAV_SPEED: float = 0.5
 const AOE_SCALAR: float = 1.05
 
 # World Constants
-const RAND_SEED: int = 42891 # 42 for fast-loading debug level
+const RAND_SEED: int = 42069 # 42 for fast-loading debug level
 const WIDTH: int = 512
 const HEIGHT: int = 512
 const CLAMP: int = 120
